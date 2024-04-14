146d18bcb3902c97de73f34b21cb4deb2f604d88
imran <azerimrane@gmail.com>
2024-04-14 21:32:46 +0200

    montaje de formulario de registro

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './register-page.component.html',
   styles: ``
 })
 export class RegisterPageComponent {
+
+  constructor(private fb: FormBuilder){
+  }
+
+  public myForm = this.fb.group({
+    name:['', [Validators.required]],
+    email:['', [Validators.required]],
+    username:['', [Validators.required]],
+    password:['', [Validators.required]],
+    password2:['', [Validators.required]]
+  })
+
+  isValidField(field:string){
+    //como estamos usando mucho esta funcion y podria cambiar mas adelante, vamos a modularizarla para tenerla en un unico lugar y poder hacer la llamada a la funcion
+  }
+
+  public onSave(){
+    this.myForm.markAllAsTouched();
+  }
 
 }
