a131fbf0263586e09d69f37c04c20d6451d18b22
imran <azerimrane@gmail.com>
2024-04-14 20:36:22 +0200

    ejercicio switches, montar formulario reactivo

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,9 +1,34 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
   styles: ``
 })
 export class SwitchesPageComponent {
+
+  constructor( private fb: FormBuilder){
+
+  }
+
+  public myForm: FormGroup = this.fb.group({
+    gender: ['F', Validators.required],
+    wantNotifications: [true, Validators.required],
+    termsAndConditions: [false, Validators.requiredTrue]
+  })
+
+  isValidField(field: string): boolean | null{
+    return this.myForm.controls[field].errors &&
+      this.myForm.controls[field].touched
+  }
+
+  onSave(){
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }else{
+      console.log(this.myForm.value);
+    }
+  }
 
 }
